a1a068710d9ca415a56413ed3afbbc837e8cf48e
Lucas Zapico <dev@lucaszapico.space>
2024-02-21 13:03:28 -0800

    refactor(comps) export from single source

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+// export { Pointer } from './Pointer';
+// export { Interactive } from './Interactive';
+export { Saturation} from './Saturation';
+export { Hue}  from './Hue';
+export { ColorPicker} from './ColorPicker';
+export { HexColorPicker } from './HexColorPicker';
+// export { HexColorInput} from './HexColorInput';
