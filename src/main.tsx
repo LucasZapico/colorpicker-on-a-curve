c47f28b482edebc12f8cd85b660cdcdfe1048437
Lucas Zapico <dev@lucaszapico.space>
2024-02-21 13:02:10 -0800

    feat(theme) add chakra wrappers

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from 'src/App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "src/App";
 
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from "src/theme/index";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "src/theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const AppWrapper = () => {
+  
+  return (
     <ChakraProvider resetCSS theme={theme}>
-    <ColorModeScript initialColorMode={theme.config?.initialColorMode} />
-    <App />
+      <ColorModeScript initialColorMode={theme.config?.initialColorMode} />
+      <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>
+);
